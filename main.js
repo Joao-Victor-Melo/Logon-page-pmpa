bb7ec131ab8551f40cfcc7298ab98a2afa77f78e
João victor <jvdrmelo@gmail.com>
2022-02-14 14:08:35 -0300

    MainJs

--- main.js
+++ main.js
@@ -1,9 +1,58 @@
-// TESTANDO JAVA SCRIPT COM "SRC" //
+// TESTANDO JAVASCRIPT COM "SCR"
+
+
+const MissionsArray = [
+    "Eyes",
+    "GET",
+    "POST",
+    "DELETE",
+    "ENVIAR DADOS DE LOGIN PARA API"
+    ]
+
+    
+
+// TESTANDO CONSOLE
 
 console.log ("Hello world")
+console.log (MissionsArray)
 
 
-// Comando para o "Hide/Show Passowrd" //
+
+// FUNÇÃO PARA API (teste)
+
+
+function fazPost(url, body) {
+    console.log("Body=", body)
+    let request = new XMLHttpRequest()
+    request.open("POST", url, true)
+    request.setRequestHeader("Conten-type", "application/json")
+    request.send(JSON,stringify(body))
+
+    request.onload = function() {
+        console.log(this.responseText)
+    }
+
+    return request.responseText
+}
+
+function cadastraUsuario () {
+    event.preventDefault ()
+    let url = "http://127.0.0.1:5500/index.html"
+    let email = document.getElementById ("email").value
+    let password = document.getElementById ("password").value
+    console.log(email)
+    console.log(password)
+
+    body = {
+        "email": email, 
+        "password": password
+    }
+
+}
+
+
+
+// Comando "Hide/Show Passoword"
 
 function myFunction() {
     var x = document.getElementById("myInput");
